adcf4357dd4ec29f9139050cb287cfe2c6ea7433
Jose08p23 <166144809+Jose08p23@users.noreply.github.com>
2024-04-05 18:01:41 +0200

    Version 0.2 de nuevo
    No parece que se haya subido los cambios al repositorio, por lo que vuelvo a subir el archivo Calculator_Inside.java de nuevo

--- Calculator_Inside/src/main/java/com/mycompany/calculator_inside/Calculator_Inside.java
+++ Calculator_Inside/src/main/java/com/mycompany/calculator_inside/Calculator_Inside.java
@@ -1,55 +1,69 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-package com.mycompany.calculator_inside;
+package calculadora;
 
 import java.util.Scanner;
 
-/**
- *
- * @author alu_tarde
- */
 public class Calculator_Inside {
 
     public static void main(String[] args) {
 
         int opc;
+        int operador1;
+        int operador2;
         Scanner sc = new Scanner(System.in);
         Calculadora c1 = new Calculadora();
-        
+
         do {
-            /*
+
             System.out.println(" Bienvenido a la calculadora elige una opción");
             System.out.println("1. Sumar");
             System.out.println("2.Restar");
             System.out.println("3.Multiplicar");
             System.out.println("4.Dividir");
-            System.out.println("5.Sumatorio");
-            System.out.println("6.Factor");
+            /*System.out.println("5.Sumatorio");
+            System.out.println("6.Factor");*/
 
             opc = sc.nextInt();
 
             switch (opc) {
 
                 case 1:
-                    c1.sumar();
+                    System.out.println("Introduce 2 numeros a operar");
+                    operador1 = sc.nextInt();
+                    operador2 = sc.nextInt();
+
+                    System.out.println("Tu resultado es: "+c1.sumar(operador1,operador2));
                     break;
 
                 case 2:
-                    c1.restar();
+                    System.out.println("Introduce 2 numeros a operar");
+                    operador1 = sc.nextInt();
+                    operador2 = sc.nextInt();
+                     System.out.println("Tu resultado es: "+     c1.restar(operador1,operador2));
+               
                     break;
 
                 case 3:
-                    c1.multiplicar();
+                    System.out.println("Introduce 2 numeros a operar");
+                    operador1 = sc.nextInt();
+                    operador2 = sc.nextInt();
+                     System.out.println("Tu resultado es: "+     c1.multiplicar(operador1,operador2));
+               
                     break;
 
                 case 4:
-                    c1.dividir();
+                    System.out.println("Introduce 2 numeros a operar");
+                    operador1 = sc.nextInt();
+                    operador2 = sc.nextInt();
+                     System.out.println("Tu resultado es: "+  c1.dividir(operador1,operador2));
+                  
                     break;
 
             }//switch end
 
         } while (opc != 0);
-*/
+
     }//main end
 }//class end
