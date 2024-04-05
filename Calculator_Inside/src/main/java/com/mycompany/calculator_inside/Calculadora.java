eaf1bbe15f44bd404498a7677fc86757e0116f43
Woolen66 <122840872+Woolen66@users.noreply.github.com>
2024-04-05 17:34:05 +0200

    version 0.0 (Joaquin Matias Aumente Peluffo)
    crear metodos sumar,restar,dividir,multiplicar Jacob mongolico

--- Calculator_Inside/src/main/java/com/mycompany/calculator_inside/Calculadora.java
+++ Calculator_Inside/src/main/java/com/mycompany/calculator_inside/Calculadora.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.calculator_inside;
+package com.mycompany.calculadora_trucha;
 
 /**
  *
@@ -10,20 +10,19 @@ package com.mycompany.calculator_inside;
  */
 public class Calculadora {
 
-    public int sumar() {
-
-        return 1;
+    public int sumar(int num1, int num2) {
+        return num1+num2;
     }
 
-    public int restar() {
-        return 2;
+    public int restar(int num1, int num2) {
+        return num1-num2;
     }
 
-    public int multiplicar() {
-        return 2;
+    public int multiplicar(int num1, int num2) {
+        return num1*num2;
     }
 
-    public int dividir() {
-
+    public int dividir(int num1, int num2) {
+        return num1/num2;
     }
 }
