6e3d3ba2271f99d092ea0371233115004fd612d3
vcj00 <154019449+vcj00@users.noreply.github.com>
2024-04-05 17:19:14 +0200

    Add files via upload

---
+++ Calculator_Inside/src/main/java/com/mycompany/calculator_inside/Calculadora.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calculator_inside;
+
+/**
+ *
+ * @author alu_tarde
+ */
+public class Calculadora {
+
+    public int sumar() {
+
+        return 1;
+    }
+
+    public int restar() {
+        return 2;
+    }
+
+    public int multiplicar() {
+        return 2;
+    }
+
+    public int dividir() {
+
+    }
+}
---
+++ Calculator_Inside/src/main/java/com/mycompany/calculator_inside/Calculator_Inside.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.calculator_inside;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alu_tarde
+ */
+public class Calculator_Inside {
+
+    public static void main(String[] args) {
+
+        int opc;
+        Scanner sc = new Scanner(System.in);
+        Calculadora c1 = new Calculadora();
+        
+        do {
+            /*
+            System.out.println(" Bienvenido a la calculadora elige una opción");
+            System.out.println("1. Sumar");
+            System.out.println("2.Restar");
+            System.out.println("3.Multiplicar");
+            System.out.println("4.Dividir");
+            System.out.println("5.Sumatorio");
+            System.out.println("6.Factor");
+
+            opc = sc.nextInt();
+
+            switch (opc) {
+
+                case 1:
+                    c1.sumar();
+                    break;
+
+                case 2:
+                    c1.restar();
+                    break;
+
+                case 3:
+                    c1.multiplicar();
+                    break;
+
+                case 4:
+                    c1.dividir();
+                    break;
+
+            }//switch end
+
+        } while (opc != 0);
+*/
+    }//main end
+}//class end
